261c9001366537bf4ea1a8cced37ac8f6443e38a
shreymahendru <shreymahendru@gmail.com>
2018-01-17 11:17:43 -0500

    add auth header when signup

--- frontend/src/actions/users.js
+++ frontend/src/actions/users.js
@@ -1,11 +1,13 @@
 import api from "./../api";
 import { userLoggedIn } from "./auth";
+import addAuthorizationHeader from "./../utils/addAuthorizationHeader";
 
 
 const signup = (creds) => (dispatch) =>
     api.user.signup(creds).then(user => {
         localStorage.tweetSearchToken = user.token;
         localStorage.tweetSearchId = user.id;
+        addAuthorizationHeader(user.token);
         dispatch(userLoggedIn(user));
     });
 
